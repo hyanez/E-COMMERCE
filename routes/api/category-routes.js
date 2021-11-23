b7cb623aa730bf2a20c3045a36caa3da38a9794e
Hugo Yanez <hugodavidyanez@gmail.com>
2021-11-22 22:46:37 -0500

    Created a put request to update a category by its ID

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -24,7 +24,7 @@ router.get("/:id", async (req, res) => {
       include: [{ model: Product }],
     });
     if (!categoryData) {
-      res.status(404).json({ message: "That ID does not exist!" });
+      res.status(404).json({ message: "No Category found with this ID" });
       return;
     }
     res.status(200).json(categoryData);
@@ -43,8 +43,24 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryData = await Category.update({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: "No Category found with this id!" });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete("/:id", async (req, res) => {
